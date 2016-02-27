7ad2621b3cf4088e569c30cdde5fb41a485dff8a
KeWu <kewu@andrew.cmu.edu>
2016-02-27 16:49:36 -0500

    add some tests in small_program

--- user/progs/small_program.c
+++ user/progs/small_program.c
@@ -1,9 +1,24 @@
 #include <simics.h>
 #include <syscall.h>
 
+void func() {
+    func();
+}
+
 int main() {
     lprintf("I am in user program!");
     lprintf("my id: %d", gettid());
+
+    /*
+    lprintf("trying to write to kernal memory:");
+    int *p = (int*)0x00100000;
+    *p = 123;
+    */
+
+    /*
+    lprintf("trying to stack overflow");
+    func();
+    */
 
     return 0;
 }
