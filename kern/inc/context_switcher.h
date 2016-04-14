8995f42c963eed7eb045c263be781a12153e8563
Jian Wang <jianwan3@andrew.cmu.edu>
2016-04-13 23:37:15 -0400

    Doc

--- kern/inc/context_switcher.h
+++ kern/inc/context_switcher.h
@@ -13,12 +13,23 @@
 
 #include <stdint.h>
 
+/** @brief Normal context switch driven by timer interupt. */
 #define OP_CONTEXT_SWITCH 0
+ /** @brief Fork and context switch to the new process. */
 #define OP_FORK 1
+/** @brief Thread_fork and context switch to the new thread. */
 #define OP_THREAD_FORK 2
+/** @brief Block the calling thread and let scheduler to 
+ *  choose the next thread to run. 
+ */
 #define OP_BLOCK 3
+/** @brief Make runable a blocked thread identified by its tcb. */
 #define OP_MAKE_RUNNABLE 4
+/** @brief Resume (wake up) a blocked thread (make runnable 
+ *  and context switch to that thread immediately) 
+ */
 #define OP_RESUME 5
+/** @brief Yield to -1 or a given tid. */
 #define OP_YIELD 6
 
 int context_switcher_init();
