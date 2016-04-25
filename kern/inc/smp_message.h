641a7fe9f48b44f8e46f7ed9452dad3c815ebae3
Jian Wang <jianwan3@andrew.cmu.edu>
2016-04-25 00:28:44 -0400

    Some doc

--- kern/inc/smp_message.h
+++ kern/inc/smp_message.h
@@ -3,6 +3,9 @@
 
 #include <simple_queue.h>
 
+/* Request data */
+
+/** @brief Message data for fork */
 typedef struct {
     void* new_thr;
     int retry_times; 
@@ -10,75 +13,94 @@ typedef struct {
     int ppid;
 } msg_data_fork_t;
 
+/** @brief Message data for wait */
 typedef struct {
     int pid;
 } msg_data_wait_t;
 
+/** @brief Message data for vanish */
 typedef struct {
     int pid;
     int ppid;
     int status;
 } msg_data_vanish_t;
 
+/** @brief Message data for set_term_color */
 typedef struct {
     int color;
 } msg_data_set_term_color_t;
 
+/** @brief Message data for print */
 typedef struct {
     int len;
     char *buf;
 } msg_data_print_t;
 
+/** @brief Message data for set_cursor_pos */
 typedef struct {
     int row;
     int col;
 } msg_data_set_cursor_pos_t;
 
+/** @brief Message data for readline */
 typedef struct {
     int len;
     char *kernel_buf;
 } msg_data_readline_t;
 
 
+/** @brief Message data for make_runnable */
 typedef struct {
     int tid;
     int next_core;
     int result;
 } msg_data_make_runnable_t;
 
+/** @brief Message data for get_cursor_pos */
 typedef struct {
     int row;
     int col;
 } msg_data_get_cursor_pos_response_t;
 
+/* Response data */
+
+/** @brief Message response data for syscalls that have one integer return 
+  * value.
+  */
 typedef struct {
     int result;
 } msg_data_response_t;
 
+/** @brief Message response data for fork */
 typedef struct {
     void* req_msg;
     int result;
 } msg_data_fork_response_t;
 
+/** @brief Message response data for wait */
 typedef struct {
     int status;
     int pid;
 }msg_data_wait_response_t;
 
+/** @brief Message response data for init_pcb */
 typedef struct {
     int pid;
 } msg_data_set_init_pcb_t;
 
+/** @brief Message response data for vanish */
 typedef struct {
     int ori_cpu;
 } msg_data_vanish_back_t;
 
+/** @brief Message response data for yield */
 typedef struct {
     int tid;
     int next_core;
     int result;
 } msg_data_yield_t;
 
+/** @brief Message type */
 typedef enum {
     FORK,           // 0
     THREAD_FORK,    // 1
@@ -93,8 +115,6 @@ typedef enum {
     GET_CURSOR_POS, // 10
     SET_INIT_PCB,   // 11
     RESPONSE,       // 12
-
-
     FORK_RESPONSE,  // 13
     WAIT_RESPONSE,  // 14
     VANISH_BACK,    // 15
@@ -102,6 +122,7 @@ typedef enum {
     NONE
 } msg_type_t;
 
+/** @brief Message type */
 typedef struct {
     /** @brief A node to enable this message be put in a queue somewhere */
     simple_node_t node;  // 12 bytes
@@ -112,9 +133,10 @@ typedef struct {
     /** @brief Type of the message: can be request or response type */
     msg_type_t type;  // 4 bytes
     /** @brief Data field of the message, can be request or response data
-     *  Use union to limit message size
+     *  Use union to limit message size.
      */
     union {
+        /* Request data */
         msg_data_fork_t fork_data;
         msg_data_wait_t wait_data;
         msg_data_vanish_t vanish_data;
@@ -125,8 +147,7 @@ typedef struct {
         msg_data_print_t print_data;
         msg_data_make_runnable_t make_runnable_data;
         msg_data_yield_t yield_data;
-
-
+        /* Response data */
         msg_data_wait_response_t wait_response_data;
         msg_data_get_cursor_pos_response_t get_cursor_pos_response_data;
         msg_data_response_t response_data;
